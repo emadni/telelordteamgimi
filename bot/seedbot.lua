f4becc7b0ae1459913193169e1121da12f2af52c
emadni <emadkarimi96@gmail.com>
2016-01-11 21:31:32 +1000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {110626080,113448163,105652025}--Sudo users
+    sudo_users = {110626080,113448163,105652025},--Sudo users
     disabled_channels = {},
     realm = {88680068},--Realms Id
     moderation = {data = 'data/moderation.json'},
